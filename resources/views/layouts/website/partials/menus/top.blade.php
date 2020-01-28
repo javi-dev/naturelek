6c94155940e7bc3dbb25dfb27a5831c3b9ede251
Javier Quintana <javier@javierquintana.es>
2020-01-28 11:51:14 +0000

    Extract top menu into a partial

---
+++ resources/views/layouts/website/partials/menus/top.blade.php
@@ -0,0 +1,51 @@
+<nav class="hidden pt-10 pb-3 font-bold text-gray-500 whitespace-no-wrap lg:flex">
+    <div class="group">
+        <div class="flex pb-2 ml-10 cursor-pointer hover:text-gray-900">
+            <div>Acceso</div>
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+        </div>
+        <div class="absolute invisible py-4 ml-10 bg-white rounded-lg shadow-lg group-hover:visible">
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">CRM</a>
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Usuarios</a>
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Telemedidas</a>
+        </div>
+    </div>
+
+    <div class="group">
+        <div class="flex pb-2 ml-10 cursor-pointer hover:text-gray-900">
+            <a href="#">Trabaja con nosotros</a>
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+        </div>
+        <div class="absolute invisible py-4 ml-10 bg-white rounded-lg shadow-lg group-hover:visible">
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Colaborador</a>
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Comercial</a>
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Naturelek
+                contigo</a>
+        </div>
+    </div>
+
+    <div class="group">
+        <div class="flex pb-2 ml-10 cursor-pointer hover:text-gray-900">
+            <div>Idiomas</div>
+            <svg class="w-6 h-6 fill-current " viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+        </div>
+        <div class="absolute invisible py-4 ml-10 bg-white rounded-lg shadow-lg group-hover:visible">
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Inglés</a>
+            <a href="#"
+                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Euskera</a>
+        </div>
+    </div>
+</nav>
