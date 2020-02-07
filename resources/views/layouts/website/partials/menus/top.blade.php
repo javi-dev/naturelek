83533c139cddfd2abc057e55f1aaae3f152b8cee
Javier Quintana <javier@javierquintana.es>
2020-02-07 11:03:36 +0000

    Right align top menu
    While we are at it, make font size smaller; right align dropdowns,
    text of the items inside them, and accent colors.

--- resources/views/layouts/website/partials/menus/top.blade.php
+++ resources/views/layouts/website/partials/menus/top.blade.php
@@ -1,54 +1,54 @@
-<nav class="hidden pt-10 pb-3 font-bold text-gray-600 whitespace-no-wrap lg:flex">
-    <div class="group">
-        <div class="flex pb-2 ml-10 cursor-pointer hover:text-gray-900">
+<nav class="hidden pt-10 pb-3 text-sm font-bold text-gray-600 whitespace-no-wrap lg:flex lg:justify-end">
+    <div class="relative text-right group">
+        <div class="flex pb-2 mr-10 cursor-pointer hover:text-gray-900">
             <div>Acceso</div>
             <svg class="w-6 h-6 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
             </svg>
         </div>
 
-        <div class="absolute invisible py-4 ml-10 bg-white rounded-lg shadow-lg group-hover:visible">
+        <div class="absolute right-0 invisible py-4 mr-10 bg-white rounded-lg shadow-lg group-hover:visible">
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">CRM</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">CRM</a>
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Usuarios</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Usuarios</a>
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Telemedidas</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Telemedidas</a>
         </div>
     </div>
 
-    <div class="group">
-        <div class="flex pb-2 ml-10 cursor-pointer hover:text-gray-900">
+    <div class="relative text-right group">
+        <div class="flex pb-2 mr-10 cursor-pointer hover:text-gray-900">
             <a href="#">Trabaja con nosotros</a>
             <svg class="w-6 h-6 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
             </svg>
         </div>
 
-        <div class="absolute invisible py-4 ml-10 bg-white rounded-lg shadow-lg group-hover:visible">
+        <div class="absolute right-0 invisible py-4 mr-10 bg-white rounded-lg shadow-lg group-hover:visible">
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Colaborador</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Colaborador</a>
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Comercial</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Comercial</a>
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Naturelek
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Naturelek
                 contigo</a>
         </div>
     </div>
 
-    <div class="group">
-        <div class="flex pb-2 ml-10 cursor-pointer hover:text-gray-900">
+    <div class="relative text-right group">
+        <div class="flex pb-2 mr-10 cursor-pointer hover:text-gray-900">
             <div>Idiomas</div>
             <svg class="w-6 h-6 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
             </svg>
         </div>
 
-        <div class="absolute invisible py-4 ml-10 bg-white rounded-lg shadow-lg group-hover:visible">
+        <div class="absolute right-0 invisible py-4 mr-10 bg-white rounded-lg shadow-lg group-hover:visible">
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Inglés</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Inglés</a>
             <a href="#"
-                class="block px-8 py-4 border-l-4 border-transparent hover:text-gray-900 hover:border-green-500">Euskera</a>
+                class="block px-8 py-4 border-r-4 border-transparent hover:text-gray-900 hover:border-green-500">Euskera</a>
         </div>
     </div>
 </nav>
