afa6e70b9c4343094596373c2ddfb17ed348d9a9
Javier Quintana <javier@javierquintana.es>
2020-02-07 10:39:56 +0000

    Add subtle shadow to header

--- resources/views/layouts/website/index.blade.php
+++ resources/views/layouts/website/index.blade.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-    <div class="bg-gray-200">
+    <div class="mb-1 bg-gray-200 shadow">
         @include('layouts.website.partials.menus.top')
 
         <h2 class="hidden text-6xl font-semibold tracking-tighter text-center text-gray-900 lg:block">Naturelek</h2>
@@ -24,7 +24,7 @@
         @include('layouts.website.partials.menus.bottom')
     </div>
 
-    <div x-data="{ open: false }" class="sticky top-0 w-full text-lg whitespace-no-wrap lg:hidden" x-cloak>
+    <div x-data="{ open: false }" class="sticky top-0 w-full mb-1 text-lg whitespace-no-wrap shadow lg:hidden" x-cloak>
         <div class="flex justify-end px-8 py-4 bg-green-500" x-bind:class="{ 'invisible': open }">
             <svg class="w-6 h-6 text-green-100 cursor-pointer fill-current hover:text-green-900" @click="open = true"
                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
