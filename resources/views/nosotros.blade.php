569dffb9a62913d33024d96bf0d3ca6b6c994591
Javier Quintana <javier@javierquintana.es>
2020-02-19 17:02:45 +0000

    Add Nuestra Experiencia section

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -58,6 +58,83 @@
                     alt="Equipo">
             </div>
         </div>
+        <div class="flex flex-wrap px-2 mt-10">
+            <div class="w-full lg:w-1/2 lg:pl-16">
+                <h1 class="text-4xl font-black tracking-tight text-gray-700">Nuestra experiencia</h1>
+                <p class="mt-6">
+                    Más de 30.000 empresas avalan el trabajo de Naturelek Consulting. Somos además la asesoría
+                    energética oficial de <a href="https://www.fvem.es/es/" class="font-bold hover:underline">FEVEM</a>
+                    (<em>¿FEVEM O FVEM?</em>) (Federación Vizcaína de Empresas del Metal), <a
+                        href="https://www.cehe.es/" class="font-bold hover:underline">Federación Española de
+                        Hostelería</a> (<em>Creo que ha cambiado de nombre a Hostelería de España</em>), <a
+                        href="https://www.ithotelero.com/" class="font-bold hover:underline">Instituto Tecnológico
+                        Hotelero</a>, <a href="http://www.cecobi.es/es/portada/"
+                        class="font-bold hover:underline">Cecobi</a>, <a href="https://www.afm.es/en/home"
+                        class="font-bold hover:underline">Advanced Manufacturing Technologies</a>, <a
+                        href="https://www.afmec.es/" class="font-bold hover:underline">AFMEC</a>, <a
+                        href="https://www.addimat.es/" class="font-bold hover:underline">ADDIMAT</a>, <a
+                        href="https://www.eskuin.com/" class="font-bold hover:underline">Eskuin</a>, <a
+                        href="https://www.descubreibiza.com/es" class="font-bold hover:underline">Fomento
+                        Turismo Ibiza</a>, y muchas más. (<em>¿Pongo enlaces a todo esto, ¿no?)</em>)
+                </p>
+            </div>
+            <div class="grid grid-cols-3 gap-4 mt-6 lg:w-1/2 lg:order-first">
+                <div class="flex items-center justify-center">
+                    <a href="https://www.fvem.es/es/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055818/naturelek/logo-fvem_muvofz.png"
+                            class="w-full grayscale" alt="FVEM">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.cehe.es/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/e_colorize:100/v1582055818/naturelek/logo_feynbz.png"
+                            class="w-full grayscale" alt="Hostelería de España">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.ithotelero.com/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055818/naturelek/logo_ITH_zwmohk.png"
+                            class="w-full grayscale" alt="Instituto Tecnológico Hotelero">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="http://www.cecobi.es/es/portada/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/e_grayscale/v1582055818/naturelek/logo-cecobi_f8ev1d.jpg"
+                            class="w-full grayscale" alt="Cecobi">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.afm.es/en/home">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055818/naturelek/AFM-Advanced-Manufacturing-Technologies_wwq7lc.svg"
+                            class="w-full grayscale" alt="Advanced Manufacturing Technologies">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.afmec.es/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055817/naturelek/logo-afmec_gfxliy.svg"
+                            class="w-full grayscale" alt="AFMEC">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.addimat.es/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055817/naturelek/addimat-logo_lzmia3.svg"
+                            class="w-full grayscale" alt="ADDIMAT">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.eskuin.com/">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055817/naturelek/eskuin_dzxhrn.svg"
+                            class="w-full grayscale" alt="Eskuin">
+                    </a>
+                </div>
+                <div class="flex items-center justify-center">
+                    <a href="https://www.descubreibiza.com/es">
+                        <img src="https://res.cloudinary.com/jaquanor/image/upload/v1582055817/naturelek/fomento_del_turismo_logo_ze8ibs.png"
+                            class="w-full grayscale" alt="Fomento Turismo Ibiza">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
