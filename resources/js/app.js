ba05f74752fc6b867ae3572ad64a45d1f49ae210
Javier Quintana <javier@javierquintana.es>
2020-02-23 16:50:10 +0000

    Add tawk.to widget to the master layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+require("./bootstrap");
+
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+(function() {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/5db0316378ab74187a5b1784/default";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+})();
